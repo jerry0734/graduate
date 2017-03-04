18ad5083b213fc7890db4839738b479cef5b3de2
Jerry Xie <jerry34@foxmail.com>
2017-03-04 09:40:33 +0800

    在settings添加markdown2和pagedown

--- blog/settings.py
+++ blog/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'markdown2',
+    'pagedown',
 )
 
 MIDDLEWARE_CLASSES = (

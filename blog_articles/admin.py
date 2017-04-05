a5db2b106d2b004a37570524149ade0c72bde70f
Jerry Xie <jerry34@foxmail.com>
2017-04-05 16:48:49 +0800

    添加编辑器（未完成）

--- blog_articles/admin.py
+++ blog_articles/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
+from django.db import models
 from .models import Aritcle, Category
+from pagedown.widgets import AdminPagedownWidget
+
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    # formfield_overrides = {
+    # models.TextField: {'widget': AdminPagedownWidget },
+    # }
+    fields = ['category', 'title', 'abstract', 'status', 'top', 'body', 'reading', 'likes']
 
-admin.site.register(Aritcle)
+    class Media:
+        js = (
+            'js/jquery-3.2.0.min.js',
+            'editormd/editormd.min.js',
+            'editormd/editormd.amd.min.js'
+        )
+        css = {
+            'all': ('editormd/css/editormd.css',),
+        }
+
+
+admin.site.register(Aritcle, ArticleAdmin)
 admin.site.register(Category)

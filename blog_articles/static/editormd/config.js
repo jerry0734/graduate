b74728c21a0ba55ebfdddedee66ab786eb075dbb
Jerry Xie <jerry34@foxmail.com>
2017-04-06 20:03:20 +0800

    静态文件配置

---
+++ blog_articles/static/editormd/config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by jerry on 005/2017/4/5.
+ */
+$(function () {
+    var editor = editormd("vLargeTextField", {
+        path: "/static/editormd/lib/" // Autoload modules mode, codemirror, marked... dependents libs path
+    });
+});

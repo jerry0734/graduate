840530850e283c947aa006d03f1df579b8791517
Jerry Xie <jerry34@foxmail.com>
2017-04-10 20:08:10 +0800

    。。

--- blog_articles/urls.py
+++ blog_articles/urls.py
@@ -4,6 +4,6 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
+                  url(r'^$', views.IndexView.as_view(), name='index'),
                   url(r'^aboutme$', views.aboutme, name='aboutme'),
-] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

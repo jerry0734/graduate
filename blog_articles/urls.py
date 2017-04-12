03e925730e88e307a3e9dbc5fde3d27ce994a0d0
Jerry Xie <jerry34@foxmail.com>
2017-04-12 13:49:18 +0800

    修bug

--- blog_articles/urls.py
+++ blog_articles/urls.py
@@ -6,6 +6,9 @@ from . import views
 urlpatterns = [
                   url(r'^$', views.IndexView.as_view(), name='index'),
                   url(r'^aboutme$', views.aboutme, name='aboutme'),
+                  url(r'^categories$', views.showcategories, name='categories'),
+                  url(r'^categories/(?P<category_id>\d+)$',
+                      views.categoryview, name='category'),
                   url(r'^article/(?P<article_id>\d+)$',
                       views.ArticleDetailView.as_view(), name='detail'),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blog_articles/views.py
+++ blog_articles/views.py
@@ -25,6 +25,7 @@ class IndexView(ListView):
 
 
 class ArticleDetailView(DetailView):
+    """文章详情页视图"""
     model = Aritcle
 
     template_name = "blog_articles/article.html"
@@ -38,6 +39,31 @@ class ArticleDetailView(DetailView):
         context = super(ArticleDetailView, self).get_object()
         return context
 
+    def get_context_data(self, **kwargs):
+        kwargs['category_list'] = Category.objects.all().order_by('name')
+        # 增加一个category_list,用于在页面显示所有分类，按照名字排序
+        return super(ArticleDetailView, self).get_context_data(**kwargs)
+
+
+# class CategoryView(ListView):
+#     template_name = "blog_articles/category.html"
+#     # 指定需要渲染的模板
+#
+#     context_object_name = "article_list"
+#
+#     # 指定模板中需要使用的上下文对象的名字
+#
+#     def get_queryset(self):
+#         # get_queryset 的作用已在第一篇中有介绍，不再赘述
+#         article_list = Aritcle.objects.filter(category=self.kwargs['category_id'], status='p')
+#         # 注意在url里我们捕获了分类的id作为关键字参数（cate_id）传递给了CategoryView，传递的参数在kwargs属性中获取。
+#         return article_list
+#
+#     # 给视图增加额外的数据
+#     def get_context_data(self, **kwargs):
+#         kwargs['category_list'] = Category.objects.all().order_by('name')
+#         # 增加一个category_list,用于在页面显示所有分类，按照名字排序
+#         return super(CategoryView, self).get_context_data(**kwargs)
 
 def aboutme(request):
     """显示关于博客的页面"""
@@ -46,3 +72,17 @@ def aboutme(request):
     about = Aboutme.objects.all()[0]
     context = {'about': about}
     return render(request, 'blog_articles/aboutme.html', context)
+
+
+def showcategories(request):
+    """分类主页面"""
+    category_list = Category.objects.all()
+    context = {'category_list': category_list}
+    return render(request, 'blog_articles/categories.html', context)
+
+
+def categoryview(request, category_id):
+    category = Category.objects.get(id=category_id)
+    article_list = category.aritcle_set.order_by('-modified_time')
+    context = {'category': category, 'article_list': article_list}
+    return render(request, 'blog_articles/category.html', context)

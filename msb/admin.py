0d782487f22d7fec4e93970621122531dd99d094
Jerry Xie <jerry34@foxmail.com>
2017-04-13 21:22:06 +0800

    添加留言板应用

--- msb/admin.py
+++ msb/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from msb.models import Message
 
 # Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['content', 'author_name', 'published_time', ]
+
+
+admin.site.register(Message)
--- msb/models.py
+++ msb/models.py
@@ -4,10 +4,27 @@ from django.db import models
 # Create your models here.
 
 class Message(models.Model):
-    email = models.EmailField()
+    """留言板model"""
 
-    author_name = models.CharField(max_length=20)
+    # 电子邮件
+    email = models.EmailField('电子邮件地址')
 
-    content = models.TextField()
+    # 留言者名称
+    author_name = models.CharField('姓名', max_length=20)
 
-    published_time = models.DateTimeField()
+    # 内容
+    content = models.TextField('正文')
+
+    # 时间
+    published_time = models.DateTimeField('发表时间', auto_now_add=True)
+
+    def __str__(self):
+        # 控制显示长度
+        if len(self.content) <= 50:
+            return self.content
+        else:
+            return self.content[:50] + "..."
+
+    class Meta:
+        ordering = ['-published_time']
+        verbose_name_plural = '留言'

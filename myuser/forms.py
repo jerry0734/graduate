6faaa2035bdd56dae4b769f0dad44596b8620490
Jerry Xie <jerry34@foxmail.com>
2017-05-01 22:28:01 +0800

    注册表单未完成

---
+++ myuser/forms.py
@@ -0,0 +1,128 @@
+import unicodedata
+from django import forms
+from django.contrib.auth import authenticate, password_validation, get_user_model
+from django.contrib.auth.tokens import default_token_generator
+from .models import allUser
+
+
+class BlogUserCreationForm(forms.ModelForm):
+    """
+    A form that creates a user, with no privileges, from the given username and
+    password.
+    https://github.com/django/django/blob/master/django/contrib/auth/forms.py
+    """
+    error_messages = {
+        'password_mismatch': "两次密码不相符",
+        'Duplicate_username': "这个用户名已经有人注册过了，请更换",
+        'Duplicate_email': "这个邮箱已经被注册过了",
+        'Duplicate_phone': "这个手机号已经被注册过了",
+    }
+
+    # 包含字母、数字和字符@/./+/-/_
+    username = forms.RegexField(
+        regex=r'^[\w.@+-]+$',
+        max_length=20,
+        error_messages={
+            'required': "用户名未输入",
+            'invalid': "用户名不合法",
+        }
+    )
+
+    password1 = forms.CharField(
+        label="密码",
+        strip=False,
+        widget=forms.PasswordInput,
+        help_text=password_validation.password_validators_help_text_html(),
+        error_messages={
+            'required': "密码未输入",
+        }
+    )
+    password2 = forms.CharField(
+        label="密码确认",
+        widget=forms.PasswordInput,
+        strip=False,
+        help_text="请输入与上面一样的密码",
+        error_messages={
+            'required': "未输入密码确认",
+        }
+    )
+
+    email = forms.EmailField(
+        label="邮箱",
+        error_messages={
+            'invalid': "email格式不正确",
+            'required': "email未输入",
+        }
+    )
+
+    phone = forms.CharField(
+        label="手机号",
+        max_length=15,
+        error_messages={
+            'invalid': "格式不对",
+            'required': "未输入手机号",
+        }
+    )
+
+    class Meta:
+        model = allUser
+        fields = ("username", "email", "phone")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self._meta.model.USERNAME_FIELD in self.fields:
+            self.fields[self._meta.model.USERNAME_FIELD].widget.attrs.update({'autofocus': True})
+
+    # 直接从源码仿过来的
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        self.instance.username = self.cleaned_data.get('username')
+        password_validation.validate_password(self.cleaned_data.get('password2'), self.instance)
+        return password2
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        existed_email = allUser._default_manager.get(email=email)
+        try:
+            existed_email
+        except allUser.DoesNotExist:
+            return email
+        raise forms.ValidationError(
+            self.error_messages['Duplicate_email'],
+            code='Duplicate_email',
+        )
+
+    def clean_phone(self):
+        phone = self.cleaned_data.get("phone")
+        try:
+            allUser._default_manager.get(phone=phone)
+        except allUser.DoesNotExist:
+            return phone
+        raise forms.ValidationError(
+            self.error_messages['Duplicate_phone'],
+            code='Duplicate_phone'
+        )
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        try:
+            allUser._default_manager.get(username=username)
+        except allUser.DoesNotExist:
+            return username
+        raise forms.ValidationError(
+            self.error_messages['Duplicate_username'],
+            code='Duplicate_username'
+        )
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
--- myuser/views.py
+++ myuser/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from .forms import BlogUserCreationForm
 from .models import allUser
 
 
@@ -28,7 +29,16 @@ def user_login(request):
 
 
 # 限制修饰符
+@login_required
 def user_logout(request):
     """用户注销"""
     logout(request)
     return HttpResponseRedirect(reverse('blog:index'))
+
+
+def register(request):
+    # if request.method == "POST":
+    if request.method != "POST":
+        form = BlogUserCreationForm()
+    else:
+        form = BlogUserCreationForm(data=request.POST)

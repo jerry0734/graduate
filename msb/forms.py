ab7902726050aaaf1f02a542769d9b7139a623c3
Jerry Xie <jerry34@foxmail.com>
2017-05-05 21:28:08 +0800

    留言板微调

--- msb/forms.py
+++ msb/forms.py
@@ -3,9 +3,14 @@ from .models import Message
 
 
 class MessageForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    author_name = forms.CharField(max_length=20)
+
+    content = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}))
+
     class Meta:
         model = Message
         fields = ['author_name', 'email', 'content']
         # 渲染到表单上的标签，空的表示不生成
         labels = {'content': ""}
-        widgets = {'content': forms.Textarea(attrs={'rows': 3})}

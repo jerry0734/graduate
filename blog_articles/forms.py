5b0f652c3079d327dca501225c3d710a0362c332
Jerry Xie <jerry34@foxmail.com>
2017-05-06 23:01:30 +0800

    评论功能（应该完成了）

--- blog_articles/forms.py
+++ blog_articles/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 
 class CommentForm(forms.ModelForm):
-    content = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'color-form-control', 'rows': 3}))
 
     class Meta:
         model = Comments
